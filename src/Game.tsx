863c550f9b18f2b0d9bbf79a68a2f7eec16fd140
Diego Cohen <diego.cohen3@gmail.com>
2023-01-01 11:47:06 -0800

    Initialize board state

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,17 +1,18 @@
-import {useState} from 'react'
-import './index.css'
+const INITIAL_BOARD_STATE = Array(9).fill("");
 
 function Game() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="h-full p-8 text-slate-800 bg-gradient-from-r from-cyan-500 to-blue-500">
+    <div className="h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500">
       <h1 className="text-center text-5xl mb-4 font-display text-white">Tic Tac Toe</h1>
       <div>
-        <div>Board</div>
+        <div>{INITIAL_BOARD_STATE.map((player, index) => (
+            <div>{player}</div>
+          ))}
+        </div>
+        <div>Scores</div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Game
+export default Game;
